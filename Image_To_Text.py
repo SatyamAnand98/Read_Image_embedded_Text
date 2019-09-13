4995d36ed776d669e11ba3940b9348cbffb74bbd
SatyamAnand98 <thesatemail@gmail.com>
2019-09-14 01:46:19 +0530

    Second commit boss

--- Image_To_Text.py
+++ Image_To_Text.py
@@ -3,5 +3,15 @@ try:
 except ImportError:
     import Image
 import pytesseract
+import sys
 
-print(pytesseract.image_to_string(Image.open('<path_to_your_image>')))
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+
+'''
+Syntax:
+image_to_data(image, lang=None, config='', nice=0, output_type=Output.STRING, timeout=0)
+'''
+
+print(pytesseract.image_to_string(Image.open('/home/stoneduser/Desktop/test.png')))
